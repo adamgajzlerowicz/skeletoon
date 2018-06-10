6056c07d62b298dce4dd358ad0f34f9d291d6fc1
Adam Gajzlerowicz <nelf86@gmail.com>
2018-06-10 16:08:27 +0200

    register mostly working

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -3,10 +3,13 @@
 import * as React from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
-// import All from 'crocks/All';
-// import mconcat from 'crocks/helpers/mconcat';
+import { Redirect } from 'react-router-dom';
+import All from 'crocks/All';
+import mconcat from 'crocks/helpers/mconcat';
 
-// const allPass = mconcat(All);
+const allPass = mconcat(All);
+
+const formatError = message => message.message.replace('GraphQL error:', '');
 
 const Register = () => {
     let username = '';
@@ -15,56 +18,48 @@ const Register = () => {
 
     return (
         <Mutation mutation={CREATE_USER}>
-            {(createUser, { data }) => (
-                <div>
-                    <form
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            createUser({ variables: {
-                                username,
-                                password,
-                                email,
-                            } });
-
-                            // username = '';
-                            // password = '';
-                            // email = '';
-                        }}
-                    >
-
-                        <input
-                            placeholder="username"
-                            ref={(node: ?HTMLInputElement) => {
-                                username = node && node.value;
+            {(createUser, { data, error }) => {
+                if (data) {
+                    return <Redirect to="/" />;
+                }
+                return (
+                    <div>
+                        <form
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                createUser({ variables: { username, password, email } });
                             }}
-                        />
-
-                        <input
-                            placeholder="email"
-                            type="email"
-                            ref={(node: ?HTMLInputElement) => {
-                                email = node && node.value;
-                            }}
-                        />
-
-                        <input
-                            placeholder="password"
-                            type="password"
-                            ref={(node: ?HTMLInputElement) => {
-                                password = node && node.value;
-                            }}
-                        />
-
-                        <button
-                            type="submit"
-                            // disabled={!allPass([username, password, email]).valueOf()}
                         >
-                            Register
-                        </button>
 
-                    </form>
-                </div>
-            )}
+                            <input
+                                placeholder="username"
+                                name="username"
+                                ref={(node: ?HTMLInputElement) => { username = node && node.value; }}
+                            />
+
+                            <input
+                                placeholder="email"
+                                type="email"
+                                ref={(node: ?HTMLInputElement) => { email = node && node.value; }}
+                            />
+
+                            <input
+                                placeholder="password"
+                                type="password"
+                                ref={(node: ?HTMLInputElement) => { password = node && node.value; }}
+                            />
+
+                            <button
+                                type="submit"
+                                disabled={!allPass([username, password, email]).valueOf()}
+                            >
+                            Register
+                            </button>
+                            {error && <div className="error"> {formatError(error)} </div>}
+                        </form>
+                    </div>
+                );
+            }}
         </Mutation>
     );
 };
