613befd17226a9109fac08adb8c85de28c036945
Adam Gajzlerowicz <nelf86@gmail.com>
2018-06-10 11:29:15 +0200

    added registration

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,7 +1,68 @@
 // @flow
 
 import * as React from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 
-const Register = () => <div> Register page </div>;
+const Register = () => {
+    let username;
+    let password;
+    let email;
+
+    return (
+        <Mutation mutation={CREATE_USER}>
+            {(createUser, { data }) => (
+                <div>
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            createUser({ variables: {
+                                username,
+                                password,
+                                email,
+                            } });
+                            // username = '';
+                            // password = '';
+                            // email = '';
+                        }}
+                    >
+
+                        <input
+                            placeholder="username"
+                            ref={(node: ?HTMLInputElement) => {
+                                username = node.value;
+                            }}
+                        />
+
+                        <input
+                            placeholder="email"
+                            ref={(node: ?HTMLInputElement) => {
+                                email = node;
+                            }}
+                        />
+
+                        <input
+                            placeholder="password"
+                            type="password"
+                            ref={(node: ?HTMLInputElement) => {
+                                password = node;
+                            }}
+                        />
+                        <button type="submit">Register</button>
+                    </form>
+                </div>
+            )}
+        </Mutation>
+    );
+};
+
+const CREATE_USER = gql`
+  mutation addTodo($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      username
+    }
+  }
+
+`;
 
 export default Register;
