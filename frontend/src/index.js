7e68a74d465ac258d6feb5b24b5d7fd421a0a10a
Adam Gajzlerowicz <nelf86@gmail.com>
2018-06-10 10:04:11 +0200

    removed redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,19 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Example from './components/example';
+import { client } from './state';
+
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
-
-const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
         <Example />
     </ApolloProvider>,
-    document.getElementById('root') || document.createElement('div'),
+    document.getElementById('root') || document.createElement('div', { id: 'root' }),
 );
---
+++ frontend/src/state/index.js
@@ -0,0 +1,14 @@
+// @flow
+
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
+export {
+    client,
+};
